69c4094f21cc309509291a72759c82c9977a4619
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-31 17:45:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "523634921869279235"; // ايدي السررفر
-var channel = "523634923068719144";//ايدي الروم
+var server = ""; // ايدي السررفر
+var channel = "";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
